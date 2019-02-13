6f6003c5ed453bd757858d12de2603aeea2b1a35
Ali Hasan <ahb7671@gmail.com>
2019-02-13 17:47:30 +0500

    Scripts ;-;
    Signed-off-by: Ali Hasan <ahb7671@gmail.com>

---
+++ psetup.sh
@@ -0,0 +1,22 @@
+# This script is for my personal setup.
+# If you want to use it, modify accordingly.
+# Especially GIT configuration.
+# Run it once.
+
+# Set up build environment
+cd ~
+sudo apt-get update
+sudo apt-get install git
+git clone https://github.com/akhilnarang/scripts
+cd scripts
+bash setup/android_build_env.sh
+
+# Misc.
+cd ~
+rm -rf scripts
+git clone https://github.com/AliHasan7671/private-scripts -b master scripts
+bash scripts/setup.sh
+
+echo Done
+
+# End
