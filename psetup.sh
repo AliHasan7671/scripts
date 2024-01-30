32ee4a1b739753969dacaf830a57692d11ea0d35
Ali Hasan <AHB7671@gmail.com>
2024-01-30 13:08:48 +0500

    Update psetup.sh

--- psetup.sh
+++ psetup.sh
@@ -4,18 +4,10 @@
 # Run it once.
 
 # Set up build environment
-cd ~
-sudo apt-get update
-sudo apt-get install git -y
-git clone git@github.com:akhilnarang/scripts.git
-cd scripts
-bash setup/android_build_env.sh
+cd ~; sudo apt-get update; sudo apt-get install git -y; git clone git@github.com:akhilnarang/scripts.git; cd scripts; bash setup/android_build_env.sh
 
 # Misc.
-cd ~
-rm -rf scripts
-git clone git@github.com:AliHasan7671/private-scripts -b master scripts
-bash scripts/setup.sh
+cd ~; rm -rf scripts; git clone git@github.com:AliHasan7671/private-scripts -b master scripts; bash scripts/setup.sh
 
 echo Done
 
